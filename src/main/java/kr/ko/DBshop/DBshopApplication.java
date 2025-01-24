310131b182e5b1f9324f9c5feed310dd13a42047
Ryu-HW <tyuy1018@gmail.com>
2025-01-24 20:25:58 +0900

    application ignore설정(기본)

---
+++ src/main/java/kr/ko/DBshop/DBshopApplication.java
@@ -0,0 +1,13 @@
+package kr.ko.DBshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DBshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ko/DBshop/DBshopApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ko.DBshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DBshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
